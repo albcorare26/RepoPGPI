9c6b5c94eed527a8a34fe93966d13bde3714b343
Alberto Cordón Arévalo <56026593+albcorare26@users.noreply.github.com>
2020-12-28 13:24:35 +0100

    Update wp-config.php

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', '3696605_beautifulteeth' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', '3696605_beautifulteeth' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'albcorare12345!' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'fdb27.runhosting.com:3306' );
